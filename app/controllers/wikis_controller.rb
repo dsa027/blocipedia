5a6e9620821b456f1c5274f85e700f34ce82e994
David Alderson <dsa027@gmail.com>
2018-02-06 14:12:12 -0700

    side-commit: cosmetics

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -15,7 +15,7 @@ class WikisController < ApplicationController
 
   def create
     @wiki = Wiki.new(wiki_params)
-    @wiki.user_id = current_user.id
+    @wiki.user = current_user
 
     if @wiki.save
       redirect_to @wiki, notice: "Wiki was saved successfully."
