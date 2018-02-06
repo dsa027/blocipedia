fe49c55b93afaa28a723734842021b0182bfec2a
David Alderson <dsa027@gmail.com>
2018-02-06 10:58:04 -0700

    Collect user's name upon sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  # after_create :send_admin_email
-
-  def send_admin_email
-    AdminMailer.new_user(self).deliver_now
-  end
 end
