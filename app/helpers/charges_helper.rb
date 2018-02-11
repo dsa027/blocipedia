20155336a24f32a3f1abeceb652f1560855f7c70
David Alderson <dsa027@gmail.com>
2018-02-11 12:45:40 -0700

    small dry fix

--- app/helpers/charges_helper.rb
+++ app/helpers/charges_helper.rb
@@ -6,6 +6,6 @@ module ChargesHelper
     Wiki.where('user_id = ? AND private = ?', current_user.id, true)
   end
   def private_to_public
-    Wiki.where('user_id = ? AND private = ?', current_user.id, true).update_all(private: false)
+    private_wikis.update_all(private: false)
   end
 end
