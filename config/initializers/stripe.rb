150768aa6ca0d6ca395ba226ab6fde0109505fda
David Alderson <dsa027@gmail.com>
2018-04-30 14:42:38 -0700

    Heroku. Figaro.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,10 +1,9 @@
 Rails.configuration.stripe = {
   publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-  secret_key: ENV['STRIPE_SECRET_KEY']
+  secret_key: ENV['STRIPE_API_KEY']
 }
 
 # Set our app-stored secret key with Stripe
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
 # Stripe.api_key = Figaro.env.secret_key
-Stripe.api_key = "sk_test_JY7itKHzmMZQAjUI9hSUjGFW"
-Stripe.setPublishableKey("sk_test_JY7itKHzmMZQAjUI9hSUjGFW")
+# Stripe.api_key = "sk_test_JY7itKHzmMZQAjUI9hSUjGFW"
